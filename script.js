d90716414670081f46d0d7edd439e07802149899
Daniel Mendez <awilddanoru@gmail.com>
2022-02-14 18:25:28 -0800

    Testing computer choice for Janken game

---
+++ script.js
@@ -0,0 +1,19 @@
+const playChoice = [
+  "Rock",
+  "Paper",
+  "Scissors"
+]
+
+const computerChoice = playChoice[Math.floor(Math.random() * playChoice.length)];
+
+function computerPlay() {
+  console.log(computerChoice);
+}
+
+// function playRound(playerSelection, computerSelection) {
+// }
+
+// function game() {
+
+// }
+
