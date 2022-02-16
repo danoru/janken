21d879641f2c71d86c05b16c6589fcd2af9a734d
Daniel Mendez <awilddanoru@gmail.com>
2022-02-16 13:07:50 -0800

    Add functionality for playing against computer

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ const playChoice = [
   "Scissors"
 ]
 
+function playerPlay() {
+  let playerSelection = alert("What hand will you play?")
+  console.log(playerSelection);
+}
+
 function computerPlay() {
-  let computerChoice = playChoice[Math.floor(Math.random() * playChoice.length)];
-  console.log(computerChoice);
+  let computerSelection = playChoice[Math.floor(Math.random() * playChoice.length)];
+  console.log(computerSelection);
 }
 
 function playRound(playerSelection, computerSelection) {
+  playerPlay();
+  computerPlay();
+  return("You chose " + playerPlay + " and the computer chose " + computerPlay + ".";
 }
 
 function game() {
-
+// Not sure what this does yet.
 }
 
